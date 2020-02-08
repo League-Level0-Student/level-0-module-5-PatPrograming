476ad72abddfd936ca3720c0f0bff574c3dcf928
League Student <league@iMac-14.attlocal.net>
2020-02-08 11:29:33 -0800

    Study :+1:

--- src/_07_skill_practice/SkillPractice.java
+++ src/_07_skill_practice/SkillPractice.java
@@ -1,5 +1,7 @@
 package _07_skill_practice;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class SkillPractice {
@@ -39,53 +41,43 @@ for (int i = 0; i < 31; i++) {
 }
 
 void skill3() { // Get a random number that is less than 20 and print it to the console 
-
-
-
+Random r=new Random();
+int x=r.nextInt(20);
+System.out.println(x);
 //Get another random number that is less than 10 and print it to the console 
-
-
-
+Random n=new Random();
+int e=n.nextInt(10);
+System.out.println(e);
 //Using a pop-up, tell the user the difference between the numbers // Hint: use subtraction 
-
-
-
+JOptionPane.showMessageDialog(null, "The difference between "+x+" and "+e+" is "+(x-e));
 }
 
 void skill4() { // In a pop-up, ask the user for the city they live in 
-
-
-
+String city= JOptionPane.showInputDialog("What city do you live in?");
 //If they answered "San Diego", tell them they live in America's Finest City 
-
-
-
+if(city.equals("San Diego")) {
+	JOptionPane.showMessageDialog(null, "You live in America's Finest City.");
+}
 //Otherwise, tell them to move to San Diego 
-
-
-
+else {
+	JOptionPane.showMessageDialog(null, "Move to San Diego.");
+}
 //Create a variable - cars - and initialize it to the number of cars your family has. // If there are 0 cars, use a pop-up to display, "I bet you use public transportation." 
-
-
-
+int cars=3;
 //If there is 1 car, use a pop-up to display the make/model of the car 
 
 
 
 //If there is more than 1 car, use a pop-up to display how many wheels the // cars have between them. 
-
+JOptionPane.showMessageDialog(null, "My cars have 12 wheels in total.");
 
 
 }
 
 void skill5() { // In a pop-up, ask the user for the name of their school 
-
-
-
+String school=JOptionPane.showInputDialog("What is the name of your school?");
 //In another pop-up, tell the user, that their school is a fantastic school. // You must include the name of the school in the message. 
-
-
-
+JOptionPane.showMessageDialog(null, school+" is a fantastic school.");
 }
 }
 
